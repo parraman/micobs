756409d2203281fc69cf040fb6cfcb977cda374f
parraman <parraman@gmail.com>
2013-11-26 17:35:53 +0100

    Removed empty folders from the class paths.
    Previously-but-no-longer-present empty folders have been removed from the
    project class paths.

--- common/es.uah.aut.srg.micobs.editor.lang.ui/src-gen/es/uah/aut/srg/micobs/lang/ui/contentassist/antlr/internal/InternalLANGParser.java
+++ common/es.uah.aut.srg.micobs.editor.lang.ui/src-gen/es/uah/aut/srg/micobs/lang/ui/contentassist/antlr/internal/InternalLANGParser.java
@@ -1,13 +1,3 @@
-/*******************************************************************************
- * Copyright (c) 2013 UAH Space Research Group.
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
- *
- * Contributors:
- *     MICOBS SRG Team - Initial API and implementation
- ******************************************************************************/
 package es.uah.aut.srg.micobs.lang.ui.contentassist.antlr.internal; 
 
 import java.io.InputStream;
@@ -20,7 +10,6 @@ import org.eclipse.xtext.parser.antlr.XtextTokenStream;
 import org.eclipse.xtext.parser.antlr.XtextTokenStream.HiddenTokens;
 import org.eclipse.xtext.ui.editor.contentassist.antlr.internal.AbstractInternalContentAssistParser;
 import org.eclipse.xtext.ui.editor.contentassist.antlr.internal.DFA;
-
 import es.uah.aut.srg.micobs.lang.services.LANGGrammarAccess;
 
 
