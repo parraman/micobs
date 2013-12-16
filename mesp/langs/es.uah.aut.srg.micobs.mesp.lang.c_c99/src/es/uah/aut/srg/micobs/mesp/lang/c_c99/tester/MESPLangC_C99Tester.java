4ba47cf59f83f31c88162dfb6fa2b6c9af87c465
parraman <parraman@gmail.com>
2013-12-16 13:04:02 +0100

    Usage of the API provided by the main language package

--- mesp/langs/es.uah.aut.srg.micobs.mesp.lang.c_c99/src/es/uah/aut/srg/micobs/mesp/lang/c_c99/tester/MESPLangC_C99Tester.java
+++ mesp/langs/es.uah.aut.srg.micobs.mesp.lang.c_c99/src/es/uah/aut/srg/micobs/mesp/lang/c_c99/tester/MESPLangC_C99Tester.java
@@ -8,20 +8,17 @@ import org.eclipse.xtext.resource.XtextResourceSet;
 
 import es.uah.aut.srg.micobs.common.MCommonPackageElement;
 import es.uah.aut.srg.micobs.common.MCommonPackageFile;
-import es.uah.aut.srg.micobs.library.LibraryManagerException;
-import es.uah.aut.srg.micobs.mesp.lang.c_c99.plugin.MESPLangC_C99Plugin;
+import es.uah.aut.srg.micobs.lang.c_c99.util.LangC_C99Util;
 import es.uah.aut.srg.micobs.mesp.mesposswi.MOSSwInterface;
 import es.uah.aut.srg.micobs.mesp.mesposswp.MOSSwPackage;
 import es.uah.aut.srg.micobs.mesp.mespswi.MSwInterface;
 import es.uah.aut.srg.micobs.mesp.mespswp.MSwPackage;
 import es.uah.aut.srg.micobs.pdl.MOperatingSystem;
 import es.uah.aut.srg.micobs.pdl.MOperatingSystemAPI;
-import es.uah.aut.srg.micobs.system.library.systemlibrary.manager.SystemLibraryManager;
+import es.uah.aut.srg.micobs.system.MLanguage;
 
 public class MESPLangC_C99Tester extends PropertyTester {
 
-	public static final String LANGUAGE_C_C99_URI = "es.uah.aut.srg.micobs.langs.C";
-	public static final String LANGUAGE_C_C99_VERSION = "C99";
 	private static final String PROPERTY_IS_C_C99 = "isC_C99";
 	
 	@Override
@@ -39,43 +36,44 @@ public class MESPLangC_C99Tester extends PropertyTester {
 			MCommonPackageFile newPack = (MCommonPackageFile)destRes.getContents().get(0);
 			MCommonPackageElement newElement = newPack.getElement();
 			
-			try {
-				if (newElement instanceof MSwPackage &&
-					((MSwPackage)newElement).getLanguages().contains(SystemLibraryManager.getLibraryManager().getElement(LANGUAGE_C_C99_URI, LANGUAGE_C_C99_VERSION)))
-				{
-					return true;
-				}
-				else if (newElement instanceof MSwInterface &&
-					((MSwInterface)newElement).getLanguage() == SystemLibraryManager.getLibraryManager().getElement(LANGUAGE_C_C99_URI, LANGUAGE_C_C99_VERSION))
-				{
-					return true;
-				}
-				else if (newElement instanceof MOSSwInterface)
-				{
-					MOSSwInterface osswi = (MOSSwInterface) newElement;
-					if (osswi.getReferencedElement() != null &&
-						osswi.getReferencedElement() instanceof MOperatingSystemAPI &&
-						((MOperatingSystemAPI)osswi.getReferencedElement()).getLanguage() ==
-						SystemLibraryManager.getLibraryManager().getElement(LANGUAGE_C_C99_URI, LANGUAGE_C_C99_VERSION))
-					{
-						return true;
-					}
-				}
-				else if (newElement instanceof MOSSwPackage)
-				{
-					MOSSwPackage osswp = (MOSSwPackage) newElement;
-					if (osswp.getReferencedElement() != null &&
-						osswp.getReferencedElement() instanceof MOperatingSystem &&
-						((MOperatingSystem)osswp.getReferencedElement()).getLanguages().contains(
-							SystemLibraryManager.getLibraryManager().getElement(LANGUAGE_C_C99_URI, LANGUAGE_C_C99_VERSION)))
-					{
-						return true;
-					}
-				}
-			} catch (LibraryManagerException e) {
-				MESPLangC_C99Plugin.INSTANCE.log(e);
+			MLanguage c_c99 = LangC_C99Util.getLanguageC_C99();
+			
+			if (c_c99 == null)
+			{
+				// Weird error has happened...
 				return false;
-			}			
+			}
+			
+			if (newElement instanceof MSwPackage &&
+				((MSwPackage)newElement).getLanguages().contains(c_c99))
+			{
+				return true;
+			}
+			else if (newElement instanceof MSwInterface &&
+				((MSwInterface)newElement).getLanguage() == c_c99)
+			{
+				return true;
+			}
+			else if (newElement instanceof MOSSwInterface)
+			{
+				MOSSwInterface osswi = (MOSSwInterface) newElement;
+				if (osswi.getReferencedElement() != null &&
+					osswi.getReferencedElement() instanceof MOperatingSystemAPI &&
+					((MOperatingSystemAPI)osswi.getReferencedElement()).getLanguage() == c_c99)
+				{
+					return true;
+				}
+			}
+			else if (newElement instanceof MOSSwPackage)
+			{
+				MOSSwPackage osswp = (MOSSwPackage) newElement;
+				if (osswp.getReferencedElement() != null &&
+					osswp.getReferencedElement() instanceof MOperatingSystem &&
+					((MOperatingSystem)osswp.getReferencedElement()).getLanguages().contains(c_c99))
+				{
+					return true;
+				}
+			}
 		}
 		return false;
 	}
