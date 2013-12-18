58a8ff3d3e877ce69049e0fe4676486767989422
parraman <parraman@gmail.com>
2013-12-18 12:28:04 +0100

    Added a utility provider class

---
+++ common/es.uah.aut.srg.micobs/src/es/uah/aut/srg/micobs/util/impl/MICOBSUtilProvider.java
@@ -0,0 +1,28 @@
+/*******************************************************************************
+ * Copyright (c) 2013 UAH Space Research Group.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     MICOBS SRG Team - Initial API and implementation
+ ******************************************************************************/
+package es.uah.aut.srg.micobs.util.impl;
+
+import es.uah.aut.srg.micobs.util.IMICOBSUtil;
+
+/**
+ * Class that incorporates and centralizes all the utility classes.
+ *
+ */
+public class MICOBSUtilProvider {
+
+	private static final IMICOBSUtil util = MICOBSUtil.getDefault();
+	
+	public static IMICOBSUtil getMICOBSUtil()
+	{
+		return util;
+	}
+
+}
